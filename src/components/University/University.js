f7d94eb2e9d4f0654e11b6b0b341e30dba97f93a
kaderabdul22 <mostafakamal9300@gmail.com>
2021-10-13 15:00:51 +0600

    created the main component

---
+++ src/components/University/University.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './University.css'
+
+const University = () => {
+    return (
+        <div className="university-container">
+            <div className="faculties-container">
+                <p>from University</p>
+            </div>
+            <div className="cart-container">
+                <p>List of Hire Faculties</p>
+            </div>
+        </div>
+    );
+};
+
+export default University;
